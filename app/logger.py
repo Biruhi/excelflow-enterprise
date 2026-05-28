1ac6dee185363c5457ec9be5c33b051a94f17ec9
Biruhi Tesfaye Abeje <biruhitesfaye@gmail.com>
2026-05-28 16:31:02 +0300

    Fixed Streamlit logging deployment issue

--- app/logger.py
+++ app/logger.py
@@ -1,16 +1,14 @@
-# =========================
-# FILE: app/logger.py
-# =========================
-
 import logging
+import os
 
+# Ensure logs directory exists
+os.makedirs("logs", exist_ok=True)
 
 logging.basicConfig(
     filename="logs/app.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
 
 def log_message(message):
 
